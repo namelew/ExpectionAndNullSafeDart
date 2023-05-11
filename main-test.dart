4e665e338b473ac4af4aed8ae48c1fb252376742
namelew <diogomaciel.cunha@gmail.com>
2023-05-11 08:30:49 -0300

    código base para testar e os conceitos de exceções em dart

---
+++ main-test.dart
@@ -0,0 +1,18 @@
+void functionOne() {
+  print("Started F01");
+  functionTwo();
+  print("Finished F01");
+}
+
+void functionTwo() {
+  print("Started F02");
+  for (int i = 0; i < 5; i++) {
+    print(i);
+  }
+  print("Finished F02");
+}
+void main(List<String> args) {
+  print("Started main");
+  functionOne();
+  print("Finished main");
+}
