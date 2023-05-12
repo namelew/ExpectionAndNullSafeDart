fdca759ecfd2c43e75953d15a5586d2380ac95af
namelew <diogomaciel.cunha@gmail.com>
2023-05-12 12:56:42 -0300

    criando exceções personalizadas para o bank controller

--- bin/dart_exceptions.dart
+++ bin/dart_exceptions.dart
@@ -18,7 +18,7 @@ void main() {
 
   // Fazendo transferência
   bool result = bankController.makeTransfer(
-      idSender: "Kako", idReceiver: "Ricarth", amount: 700);
+      idSender: "Kako", idReceiver: "Ricarth", amount: 500);
 
   // Observando resultado
   print(result);
